bc6484e73be544c7ba51ea30437bd13d68d55681
Itamar Ratson <itamar.ratson@gmail.com>
2024-08-10 20:09:19 +0300

    added chat top

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -2,9 +2,24 @@ import './chat.css';
 
 const Chat = () => {
 	return (
-		<>
-			<div className='chat'>Chat</div>
-		</>
+		<div className='chat'>
+			<div className='top'>
+				<div className='user'>
+					<img src='./avatar.png' alt='userImage' />
+					<div className='text'>
+						<span>Jane Doe</span>
+						<p>Lorem ipsum, dolor sit amet.</p>
+					</div>
+				</div>
+				<div className='icons'>
+					<img src='./phone.png' alt='phoneIcon' />
+					<img src='./video.png' alt='videoIcon' />
+					<img src='./info.png' alt='infoIcon' />
+				</div>
+			</div>
+			<div className='center'></div>
+			<div className='bottom'></div>
+		</div>
 	);
 };
 
