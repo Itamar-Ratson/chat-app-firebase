184e0f49e2b29d445a048158fa75a5543cfb581c
Itamar Ratson <itamar.ratson@gmail.com>
2024-08-10 03:52:40 +0300

    added base chatList styles

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,10 +1,58 @@
+import { useState } from 'react';
 import './chatList.css';
 
 const ChatList = () => {
+	const [addMode, setAddMode] = useState(false);
 	return (
-		<>
-			<div className='chatList'>ChatList</div>
-		</>
+		<div className='chatList'>
+			<div className='search'>
+				<div className='searchBar'>
+					<img src='/search.png' alt='searchImage' />
+					<input type='text' placeholder='Search' />
+				</div>
+				<img
+					className='add'
+					src={addMode ? './minus.png' : './plus.png'}
+					alt='AddButton'
+					onClick={() => setAddMode((mode) => !mode)}
+				/>
+			</div>
+			<div className='item'>
+				<img src='./avatar.png' alt='userAvatar' />
+				<div className='texts'>
+					<span>Jane Doe</span>
+					<p>Hello, world!</p>
+				</div>
+			</div>
+			<div className='item'>
+				<img src='./avatar.png' alt='userAvatar' />
+				<div className='texts'>
+					<span>Jane Doe</span>
+					<p>Hello, world!</p>
+				</div>
+			</div>
+			<div className='item'>
+				<img src='./avatar.png' alt='userAvatar' />
+				<div className='texts'>
+					<span>Jane Doe</span>
+					<p>Hello, world!</p>
+				</div>
+			</div>
+			<div className='item'>
+				<img src='./avatar.png' alt='userAvatar' />
+				<div className='texts'>
+					<span>Jane Doe</span>
+					<p>Hello, world!</p>
+				</div>
+			</div>
+			<div className='item'>
+				<img src='./avatar.png' alt='userAvatar' />
+				<div className='texts'>
+					<span>Jane Doe</span>
+					<p>Hello, world!</p>
+				</div>
+			</div>
+		</div>
 	);
 };
 
