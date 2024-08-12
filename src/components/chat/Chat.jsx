0d7d7dd5c7168dacf9202cea40a7c7e1b57e8d37
Itamar Ratson <itamar.ratson@gmail.com>
2024-08-12 19:20:37 +0300

    added base chat bottom

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
 import './chat.css';
+import EmojiPicker from 'emoji-picker-react';
 
 const Chat = () => {
+	const [open, setOpen] = useState(false);
+	const [text, setText] = useState('');
+	const handleEmoji = (event) => {
+		setText((t) => t + event.emoji);
+		setOpen(false);
+	};
+
 	return (
 		<div className='chat'>
 			<div className='top'>
@@ -18,7 +27,36 @@ const Chat = () => {
 				</div>
 			</div>
 			<div className='center'></div>
-			<div className='bottom'></div>
+			<div className='bottom'>
+				<div className='icons'>
+					<img src='./img.png' alt='image' />
+					<img src='./camera.png' alt='camera' />
+					<img src='./mic.png' alt='mic' />
+				</div>
+				<input
+					type='text'
+					placeholder='Type a message...'
+					value={text}
+					onChange={(e) => setText(e.target.value)}
+				/>
+				<div className='emoji'>
+					<img src='./emoji.png' alt='emojis' onClick={() => setOpen((open) => !open)} />
+					<div className='picker'>
+						<EmojiPicker
+							customEmojis={[
+								{
+									names: ['Primal Rage'],
+									imgUrl: 'https://wiki.guildwars.com/images/d/d8/Primal_Rage.jpg',
+									id: '[primal rage]',
+								},
+							]}
+							open={open}
+							onEmojiClick={handleEmoji}
+						/>
+					</div>
+				</div>
+				<button className='sendButton'>Send</button>
+			</div>
 		</div>
 	);
 };
